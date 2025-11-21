a1493b8389cee5002055afb694fde4cda80820f4
Alexey Mozhaev <lxmzhv@gmail.com>
2025-11-21 03:02:55 +0100

    Add constants

---
+++ guild/constants.js
@@ -0,0 +1,173 @@
+const TEAMS = ["leia", "jabba", "amidala", "drevan", "gas", "geos", "gg", "gungans", "reva", "slkr"];
+
+const ASSAULT_CHARACTERS = [
+    'padawanobiwan',
+    'masterquigon',
+    'captainenoch',
+    'deathtrooperperidea',
+    'nighttrooper'
+];
+
+const PLATOON_TO_ROSTER_ID_MAP = {
+    'arc170clonesergeant': 'clonesergeantphasei'
+};
+
+const UNIT_NAME_MAP = {
+    "clonesergeantphasei": "Clone Sergeant",
+    "ahsokatanosjedistarfighter": "Ahsoka's Starfighter", "anakinseta2starfighter": "Anakin's Starfighter",
+    "b28extinctionclassbomber": "B-28 Bomber", "btlbywingstarfighter": "BTL-B Y-wing", "biggsdarklighterxwing": "Biggs' X-wing",
+    "bistansuwing": "Bistan's U-wing", "cassiansuwing": "Cassian's U-wing", "chimaera": "Chimaera",
+    "clonesergeantsarc170": "Clone Sergeant's ARC-170", "comeuppance": "Comeuppance", "ebonhawk": "Ebon Hawk",
+    "emperorsshuttle": "Emperor's Shuttle", "endurance": "Endurance", "executor": "Executor", "executrix": "Executrix",
+    "finalizer": "Finalizer", "firstordersftiefighter": "FO SF TIE Fighter", "firstordertiefighter": "FO TIE Fighter",
+    "furyclassinterceptor": "Fury-class Interceptor", "gauntletstarfighter": "Gauntlet Starfighter",
+    "geonosiansoldiersstarfighter": "Geonosian Soldier's Starfighter", "geonosianspysstarfighter": "Geonosian Spy's Starfighter",
+    "ghost": "Ghost", "hansmillenniumfalcon": "Han's Millennium Falcon", "homeone": "Home One", "houndstooth": "Hound's Tooth",
+    "hyenabomber": "Hyena Bomber", "ig2000": "IG-2000", "imperialtiebomber": "Imperial TIE Bomber",
+    "imperialtiefighter": "Imperial TIE Fighter", "jediconsularsstarfighter": "Jedi Consular's Starfighter",
+    "kylorenscommandshuttle": "Kylo's Command Shuttle", "landosmillenniumfalcon": "Lando's Millennium Falcon",
+    "leviathan": "Leviathan", "mg100starfortresssf17": "MG-100 StarFortress", "malevolence": "Malevolence",
+    "marauder": "Marauder", "markviinterceptor": "Mark VI Interceptor", "negotiator": "Negotiator", "outrider": "Outrider",
+    "phantomii": "Phantom II", "plokoonsjedistarfighter": "Plo Koon's Starfighter", "poedameronsxwing": "Poe's X-wing",
+    "profundity": "Profundity", "punishingone": "Punishing One", "raddus": "Raddus", "ravensclaw": "Raven's Claw",
+    "razorcrest": "Razor Crest", "rebelbwing": "B-wing", "rebelywing": "Y-wing", "resistancexwing": "Resistance X-wing",
+    "rexsarc170": "Rex's ARC-170", "reysmillenniumfalcon": "Rey's Millennium Falcon", "rogueone": "Rogue One",
+    "scimitar": "Scimitar", "scythe": "Scythe", "sithfighter": "Sith Fighter", "slavei": "Slave I",
+    "sunfacsgeonosianstarfighter": "Sun Fac's Starfighter", "tieadvancedx1": "TIE Advanced x1", "tiedagger": "TIE Dagger",
+    "tiedefender": "TIE Defender", "tieechelon": "TIE Echelon", "tiereaper": "TIE Reaper", "tiesilencer": "TIE Silencer",
+    "tieininterceptorprototype": "TIE Interceptor Prototype", "umbaranstarfighter": "Umbaran Starfighter",
+    "vulturedroid": "Vulture Droid", "wedgeantillesxwing": "Wedge's X-wing", "xanadublood": "Xanadu Blood",
+    "glrey": "Rey", "supremeleaderkyloren": "SLKR", "grandmasterluke": "JML", "sithpalpatine": "SEE",
+    "jedimasterkenobi": "JMK", "lordvader": "LV", "glleia": "Leia", "jabbathehutt": "Jabba",
+    "glahsokatano": "Ahsoka", "glhondo": "Hondo", "capitalleviathan": "Levi", "capitalprofundity": "Prof",
+    "capitalexecutor": "Exec", "punishingone": "PO", "marauder": "Marauder", "badbatchhunter": "Hunter",
+    "badbatchtech": "Tech", "badbatchwrecker": "Wrecker", "tieinterceptor": "TIE Int",
+    "commanderahsoka": "CAT", "mauls7": "Maul", "maul": "Darth Maul", "bobafettscion": "Boba Fett, Scion of Jango", "darthmalgus": "Malgus",
+    "trench": "Trench", "darthbane": "Bane", "queenamidala": "Amidala", "luthenrael": "Luthen",
+    "ezraexile": "Ezra", "darkrey": "DRey", "sm33": "SM33", "jocastanu": "Jocasta", "mazkanata": "Maz",
+    "bensolo": "Ben Solo", "taronmalicos": "Taron Malicos", "moffgideons3": "Moff Gideon S3",
+    "jediknightluke": "JKL", "generalskywalker": "GAS", "wampa": "Wampa", "hermityoda": "Hermit",
+    "darthrevan": "DRevan", "bastilashanfallen": "BSF", "darthmalak": "Malak", "kiadimundi": "KAM",
+    "monmothma": "Mon Mothma", "chewbacca": "Chewie", "c3po": "C-3PO", "padmeamidala": "Padme",
+    "grandmofftarkin": "Tarkin", "sabinewren": "Sabine", "kananjarrus": "Kanan", "herasyndulla": "Hera",
+    "chopper": "Chopper", "zeborrelios": "Zeb", "bodhirook": "Bodhi", "pao": "Pao", "bistan": "Bistan",
+    "cassianandor": "Cassian", "k2so": "K-2SO", "jynerso": "Jyn", "chirrutimwe": "Chirrut", "bazemalbus": "Baze",
+    "dengar": "Dengar", "bossk": "Bossk", "ig88": "IG-88", "bobafett": "Boba Fett", "greedo": "Greedo",
+    "gamorreanguard": "Gam Guard", "mobenfocer": "Mob Enforcer", "nightsisterspirit": "Spirit",
+    "nightsisterzombie": "Zombie", "motherdalzin": "Talzin", "olddaka": "Daka", "talia": "Talia",
+    "nightsisterinitiate": "Initiate", "nightsisteracolyte": "Acolyte", "rose": "Rose Tico",
+    "amilynholdo": "Holdo", "reyjakku": "Scav Rey", "finn": "Finn", "poedameron": "Poe", "resistancepilot": "Res Pilot",
+    "resistancetrooper": "Res Trooper", "reyjeditraining": "RJ",
+    "padawanobiwan": "P. Obi-Wan",
+    "masterquigon": "M. Qui-Gon",
+    "captainenoch": "Enoch",
+    "deathtrooperperidea": "DT (Peridea)",
+    "nighttrooper": "Nighttrooper",
+    "greatmothers": "Great Mothers",
+    "r2d2_legendary": "R2-D2", "captaindrogan": "Drogan", "admiralraddus": "Raddus"
+};
+
+const OMICRON_SKILL_MAP = {
+    'generalsyndulla': [
+        { skillId: 'uniqueskill_GENERALSYNDULLA01', displayName: 'unique 1' }
+    ],
+    'padawansabine': [
+        { skillId: 'uniqueskill_PADAWANSABINE01', displayName: 'unique 1' }
+    ],
+    'greatmothers': [
+        { skillId: 'leaderskill_GREATMOTHERS', displayName: 'leader' },
+        { skillId: 'uniqueskill_GREATMOTHERS01', displayName: 'unique 1' }
+    ],
+    'jynerso': [
+        { skillId: 'uniqueskill_JYNERSO01', displayName: 'unique 1' },
+    ],
+};
+
+const RANK_MAP = {
+    'GUILD_LEADER': 'leader',
+    'GUILD_OFFICER': 'officer',
+    'GUILD_MEMBER': 'member',
+};
+
+const GALACTIC_LEGENDS_MAP = {
+    "glrey": "Rey",
+    "supremeleaderkyloren": "SLKR",
+    "grandmasterluke": "JML",
+    "sithpalpatine": "SEE",
+    "jedimasterkenobi": "JMK",
+    "lordvader": "LV",
+    "glleia": "Leia",
+    "jabbathehutt": "Jabba",
+    "glahsokatano": "Ahsoka",
+    "glhondo": "Hondo"
+};
+
+const SHIPS_MAP = {
+    "capitalleviathan": "Levi",
+    "capitalprofundity": "Prof",
+    "capitalexecutor": "Exec"
+};
+
+const PILOTS_MAP = {
+    "badbatchhunter": "Hunter",
+    "badbatchtech": "Tech",
+    "badbatchwrecker": "Wrecker"
+};
+
+const CONQUEST_CHARACTERS_SET = new Set([
+    "mauls7",
+    "bobafettscion",
+    "darthmalgus",
+    "trench",
+    "darthbane",
+    "queenamidala",
+    "luthenrael",
+    "ezraexile",
+    "darkrey",
+    "sm33",
+    "jocastanu",
+    "mazkanata",
+    "bensolo",
+    "taronmalicos",
+    "moffgideons3",
+]);
+
+const CONQUEST_SHIPS_MAP = {
+    "scythe": "Scythe",
+    "furyclassinterceptor": "Fury"
+};
+
+const CONQUEST_UNITS_ORDER = [
+    "mauls7", "bobafettscion", "scythe",
+    "darthmalgus", "bensolo", "furyclassinterceptor", "taronmalicos", "moffgideons3",
+    "trench", "darthbane", "queenamidala", "luthenrael",
+    "ezraexile", "darkrey", "sm33", "jocastanu", "mazkanata"
+];
+
+const LEIA_TEAM_UNITS = ["glleia", "r2d2_legendary", "captaindrogan", "admiralraddus", "jynerso"];
+const JABBA_TEAM_UNITS = ["jabbathehutt", "embo", "krrsantan", "boushh", "undercoverlando"];
+
+const REQ_TEAM_UNIT_COLOR_THRESHOLDS = {
+    "glleia": {
+        red: 5, orange: 7, yellow: 9, green: Infinity
+    },
+    "captaindrogan": {
+        red: 3, orange: 5, yellow: 8, green: Infinity
+    },
+    "r2d2_legendary": {
+        red: 1, orange: 3, yellow: 5, lightgreen: 7, green: Infinity
+    },
+    "jynerso": {
+        red: 1, orange: 3, yellow: 5, lightgreen: 7, green: Infinity
+    },
+    "admiralraddus": {
+        red: 1, orange: 3, yellow: 5, lightgreen: 7, green: Infinity
+    },
+    "jabbathehutt": { red: 5, orange: 7, yellow: 9, green: Infinity },
+    "embo": { red: 5, orange: 7, yellow: 9, green: Infinity },
+    "krrsantan": { red: 5, orange: 7, yellow: 9, green: Infinity },
+    "boushh": { red: 3, orange: 5, yellow: 8, green: Infinity },
+    "undercoverlando": { red: 1, orange: 3, yellow: 5, lightgreen: 7, green: Infinity }
+};
+
+const GUILD_FILES = ['guild_LVmIG5W_RSCmvZYOxdyH_Q.json', 'guild_IVgWpcsTSgKbtd7uoTiTAg.json', 'guild_RiWsgx3BQXqZMepXXEb2Cg.json', 'guild_oy2awPmqRYyCKPvgJNjPIQ.json'];
