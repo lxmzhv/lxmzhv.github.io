7d08d80f347cd7dd85b32599d1d24a377b2dc078
Alexey Mozhaev <lxmzhv@gmail.com>
2025-12-09 01:06:29 +0100

    Add a guild

--- guild/js/constants.js
+++ guild/js/constants.js
@@ -174,4 +174,5 @@ export const GUILD_FILES = [
     'guild_CryCXYUbSm2dWZhTI1LwvA.json',
     'guild_OjLlFWggT5yuH0ufYws1fA.json',
     'guild_0ftcV359S4KiFpZ9Sl0kVQ.json',
+    'guild_jrl9Q-_CRDGdMyNjTQH1rQ.json',
 ];
